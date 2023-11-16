9228eb367b261cf0257e4cc7a3d158a6cc98e850
Arshad <120387836+Arshadsyedbasha@users.noreply.github.com>
2023-11-16 14:47:56 +0530

    Add files via upload

---
+++ Day4/reactapp1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Elistitems from './components/EmployeeList/list';
+
+function App() {
+  return (
+    <div className="App">
+      <Elistitems/>
+    </div>
+  );
+}
+
+export default App;
